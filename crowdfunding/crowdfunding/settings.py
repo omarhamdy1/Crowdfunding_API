7644c64cf46f3c064fbcfc733f4ee035de4cb13c
meteopavel <isu@meteopavel.ru>
2025-04-21 16:06:15 +0800

    fix: resolve issues with email sending and user registration

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -49,9 +49,11 @@ CORS_ALLOWED_ORIGINS = [
     "https://crowdfunding.meteopavel.space",
 ]
 
-SECURE_PROXY_SSL_HEADER = os.getenv('SECURE_PROXY_SSL_HEADER') == (
+SECURE_PROXY_SSL_HEADER = (
     'HTTP_X_FORWARDED_PROTO', 'https'
-)
+    ) if os.getenv(
+        'SECURE_PROXY_SSL_HEADER'
+    ) == 'True' else None
 SECURE_SSL_REDIRECT = os.getenv('SECURE_SSL_REDIRECT') == 'True'
 SESSION_COOKIE_SECURE = os.getenv('SESSION_COOKIE_SECURE') == 'True'
 CSRF_COOKIE_SECURE = os.getenv('CSRF_COOKIE_SECURE') == 'True'
